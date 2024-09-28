179e1fcd7fac243a03ccd55d2d2bfa5a5065786d
Anna Kuprym <annakupriyanchik99@gmail.com>
2024-09-28 21:43:09 +0300

    embedchain config

--- chat_with_pdf.py
+++ chat_with_pdf.py
@@ -7,3 +7,33 @@ from streamlit_chat import message
 
 st.title("Chat with PDF using Llama 3.2")
 st.caption("This app allows you to chat with a PDF using Llama 3.2 running locally with Ollama!")
+
+def embedchain_bot(dn_path):
+    return App.from_config(
+    config = {
+        'llm': {
+            'provider': 'ollama',
+            'config': {
+                'model': 'llama2',
+                "max_tokens": 250,
+                'temperature': 0.5,
+                'top_p': 1,
+                'stream': true,
+                'base_url': 'http://localhost:11434'
+            }
+        },
+        "vectordb": {
+            "provider": "chroma",
+            "config": {
+                "dir": db_path
+            }
+        },
+        'embedder': {
+            'provider': 'ollama',
+            'config': {
+                'model': 'llama2',
+                'base_url': 'http://localhost:11434'
+            }
+        }
+    }
+    )
